156bd23cbd8440fc9eba9ca1a582549358d35e54
Arseniy <62847396+Arsenyi1986@users.noreply.github.com>
2023-07-18 22:14:12 +0300

    Add files via upload

---
+++ Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication5.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
+    public string? Title { get; set; } = string.Empty;
+
+    [Display(Name = "Release Date")]
+    //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [Required]
+    [StringLength(30)]
+    public string? Genre { get; set; } = string.Empty;
+
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+}
+//[Range(typeof(DateTime), "1/1/1966", "1/1/2020")]
---
+++ Models/SeedData.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication5.Data;
+
+namespace WebApplication5.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new WebApplication5Context(
+                serviceProvider.GetRequiredService<DbContextOptions<WebApplication5Context>>()))
+            {
+                if (context == null || context.Movie == null) 
+                {
+                    throw new ArgumentNullException("Null_Webapp5context");
+                }
+
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+                context.Movie.AddRange(
+                        new Movie
+                        {
+                             Title = "Film1",
+                             ReleaseDate = DateTime.Parse("22-09-1987"),
+                             Genre = "Romantic Comedy",
+                             Price = 12.80M
+                        },
+                        new Movie
+                        {
+                             Title = "Film2",
+                             ReleaseDate = DateTime.Parse("02-09-1883"),
+                             Genre = "Brutal Dramma",
+                             Price = 17.30M
+                        }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
